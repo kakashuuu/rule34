916bb49c25aefc19ce37a7fd5e9a78cdd4f01b71
Kakashi hatake <105074780+kakashuuu@users.noreply.github.com>
2025-06-23 12:18:47 +0530

    Update index.ts

--- src/utils/scrapers/rule34/index.ts
+++ src/utils/scrapers/rule34/index.ts
@@ -1,16 +1,22 @@
-import {load} from "cheerio";
-import {resolvedImages} from "./images";
+import { load } from "cheerio";
 import axios from "axios";
+import { resolvedImages } from "./images";
 
-export const scraper = async(search) => {
-    const headers = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 12; SM-S906N Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, como Gecko) Versão/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36",
-    }
-    const response = await axios.get(`https://www.rule34.us/index.php?r=posts/index&q=${search}`, {headers});
-    let $ = load(`${response.data}`);
-    let data: any[] = []
-    const images = $(".thumbail-container a").map((index, element) => {
-        data.push(`${$(element).attr("href")}`);
-    });
-    return resolvedImages(data);
-}
+export const scraper = async (search: string) => {
+  const headers = {
+    "User-Agent":
+      "Mozilla/5.0 (Linux; Android 12; SM-S906N Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36",
+  };
+
+  const searchURL = `https://rule34.xxx/index.php?page=post&s=list&tags=${encodeURIComponent(search)}`;
+  const response = await axios.get(searchURL, { headers });
+  const $ = load(response.data);
+  const postLinks: string[] = [];
+
+  $("span.thumb a").each((_, el) => {
+    const href = $(el).attr("href");
+    if (href) postLinks.push(`https://rule34.xxx/${href}`);
+  });
+
+  return resolvedImages(postLinks);
+};
